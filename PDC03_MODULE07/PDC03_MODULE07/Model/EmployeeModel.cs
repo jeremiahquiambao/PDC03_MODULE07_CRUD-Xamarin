fa738a2179647dfc7ceb2984566204d6b78b580c
jeremiahquiambap <quiambao.jeremiah@auf.edu.ph>
2023-06-02 12:22:34 +0800

    Add project files.

---
+++ PDC03_MODULE07/PDC03_MODULE07/Model/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC03_MODULE07.Model
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
